27ed98d368488aa12bd3ddd69a7260d34577a859
aiaturmullick <aiatur@zeron.one>
2023-06-20 21:54:44 +0530

    modified by ayat

---
+++ src/Pages/Layout/index.js
@@ -0,0 +1,167 @@
+import React, { useEffect, useState } from "react";
+import CardOne, { CardTwo } from "../../Components/Card";
+import supabase from "../../config/supabaseClient";
+// import { Cart } from '../../Context';
+import { useNavigate } from "react-router-dom";
+import Paybutton from "../../Components/Paybutton";
+export default function Layout() {
+  // const {value,setValue} = useContext(Cart);
+
+  const [error, setError] = useState(null);
+  const [data, setData] = useState([]);
+  const [extraRow, setExtraRow] = useState(false);
+  const [counterValue, setCounterValue] = useState(0);
+  const [counterValueTwo, setCounterValueTwo] = useState(0);
+  const [totalValue, setTotalValue] = useState(0);
+  const navigate = useNavigate();
+  let price, discount, currency, discountedPrice, addOns, totalPrice;
+  let emailId = localStorage.getItem("email");
+  useEffect(() => {
+    const fetchData = async () => {
+      const { data, error } = await supabase
+        .from("tb1")
+        .select("discount,Currency,addOns,voucherPrice")
+        .eq("EmailID", emailId);
+      if (error) {
+        setError("could not fetch Data!");
+        setData([]);
+      }
+      if (data) {
+        setData(data);
+        setError(null);
+      }
+    };
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    if (counterValueTwo > 0) {
+      totalPrice = discountedPrice + addOns;
+      setTotalValue(totalPrice);
+    } else {
+      totalPrice = discountedPrice;
+      setTotalValue(totalPrice);
+    }
+  }, [counterValue, counterValueTwo]);
+
+  const handleDataOne = (data) => {
+    setCounterValue(data);
+  };
+
+  const handleDataTwo = (data) => {
+    setCounterValueTwo(data);
+  };
+
+  if (data.length > 0) {
+    price = data[0].voucherPrice;
+    discount = data[0].discount;
+    currency = data[0].Currency;
+    addOns = data[0].addOns;
+    discountedPrice = Math.floor(price - discount);
+  }
+
+  return (
+    <React.Fragment>
+      <section>
+        <div className="w-screen h-screen grid grid-rows-2 text-white md:grid-cols-2">
+          <div className="w-full h-full bg-gray-100 md:h-screen container">
+            <div className="px-8">
+              <header>
+                <div class="container mx-auto px-4 py-6 ">
+                  <h1 class="text-2xl font-normal text-black border-b border-black">
+                    Early Bird Tickets | South Asia Member Registrations Open at
+                    SALC
+                  </h1>
+                </div>
+              </header>
+              <CardOne
+                title="Member (Early Bird)"
+                description={`Your voucher of ${currency} ${discount} has been applied as a discount on the actual ticket cost ${currency} ${price}`}
+                discountedPrice={`${currency} ${discountedPrice} incl. 18% GST`}
+                sendData={handleDataOne}
+                counterData={counterValueTwo}
+              />
+              <CardTwo
+                title="Spouse/Life Partner (Early Bird)"
+                description={`We have some great experiences for everyone at RIE and can help curate a holiday 
+              for you and your SLP before or after RIE while you are in India.`}
+                subTitle={`Bring along your Spouse / Life Partner to India!`}
+                discountedPrice={`${currency} ${addOns} incl. 18% GST`}
+                sendData={handleDataTwo}
+              />
+            </div>
+          </div>
+
+          <div className="w-full h-full bg-gray-200 md:h-screen container">
+            {/* 
+             Shopping Cart
+             Purchase Overview
+             */}
+            <div className="w-full h-full">
+              {/* <Addtocart /> */}
+              <div class="text-gray-500 min-h-screen">
+                <header class="bg-white shadow-md">
+                  <div class="container mx-auto px-4 py-6">
+                    <h1 class="text-2xl font-bold">Shopping Cart</h1>
+                  </div>
+                </header>
+
+                <main class=" mx-auto px-4 py-6">
+                  {counterValue > 0 ? (
+                    <>
+                      <div class="bg-white shadow w-full p-6">
+                        <div class="flex justify-between items-center mb-4">
+                          <h2 class="text-xl text-black font-semibold">
+                            Members (Early Bird)
+                          </h2>
+                          <span class="text-black">
+                            {currency} {discountedPrice}
+                          </span>
+                        </div>
+                        <hr class="my-4" />
+                        {counterValueTwo > 0 ? (
+                          <div class="flex justify-between items-center mb-4">
+                            <h2 class="text-xl text-black font-semibold">
+                              Spouse/Life Partner (Early Bird)
+                            </h2>
+                            <span class="text-black">
+                              {currency} {addOns}
+                            </span>
+                          </div>
+                        ) : null}
+                        <div class="flex justify-between items-center mb-4">
+                          <h2 class="text-xl text-black font-semibold">Fees</h2>
+                          <span class="text-black">{currency} 10.00</span>
+                        </div>
+                        <hr class="my-4" />
+                        <div class="flex justify-between items-center">
+                          <h2 class="text-2xl font-semibold">Total</h2>
+                          <span class="text-2xl text-black">
+                            {currency} {totalValue}
+                          </span>
+                        </div>
+                        <div class="flex justify-center mt-6">
+                          {/* <button class="bg-black hover:bg-gray-700 text-white font-bold py-2 px-4 rounded w-1/2 text-center"
+                          onClick={handleSubmit}>
+                          Proceed to payment
+                          </button> */}
+                          <Paybutton amount={totalValue} user={emailId} />
+                        </div>
+                      </div>
+                    </>
+                  ) : (
+                    <>
+                      <div class="bg-white shadow w-full p-6">
+                        The shopping cart is empty. Please select tickets.
+                      </div>
+                    </>
+                  )}
+                </main>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </React.Fragment>
+  );
+}
--- src/Pages/Login/index.js
+++ src/Pages/Login/index.js
@@ -1,29 +1,34 @@
-import React,{useState} from 'react';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import CssBaseline from '@mui/material/CssBaseline';
-import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
-import Link from '@mui/material/Link';
-import Grid from '@mui/material/Grid';
-import Box from '@mui/material/Box';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import supabase from '../../config/supabaseClient';
-import { useNavigate } from 'react-router-dom';
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Avatar from "@mui/material/Avatar";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Checkbox from "@mui/material/Checkbox";
+import Container from "@mui/material/Container";
+import CssBaseline from "@mui/material/CssBaseline";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Grid from "@mui/material/Grid";
+import Link from "@mui/material/Link";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import TextField from "@mui/material/TextField";
+import Typography from "@mui/material/Typography";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import supabase from "../../config/supabaseClient";
 
 function Copyright(props) {
   return (
-    <Typography variant="body2" color="text.secondary" align="center" {...props}>
-      {'Copyright © '}
+    <Typography
+      variant="body2"
+      color="text.secondary"
+      align="center"
+      {...props}
+    >
+      {"Copyright © "}
       <Link color="inherit" href="https://mui.com/">
         Your Website
-      </Link>{' '}
+      </Link>{" "}
       {new Date().getFullYear()}
-      {'.'}
+      {"."}
     </Typography>
   );
 }
@@ -33,53 +38,45 @@ function Copyright(props) {
 const defaultTheme = createTheme();
 
 export default function SignIn() {
-
   const [email, setEmail] = useState(" ");
-  const [checked,setChecked] = useState(false);
+  const [checked, setChecked] = useState(false);
   const [emailError, setEmailError] = useState(" ");
   const [error, setError] = useState(" ");
   // const [data, setData] = useState([]);
   const navigate = useNavigate();
 
-  
-    const fetchData = async () => {
-      const { data, error } = await supabase
-          .from('tb1')
-          .select('EmailID')
+  const fetchData = async () => {
+    const { data, error } = await supabase.from("tb1").select("*");
+    // .eq("emailId", email);
 
-        if(error){
-          console.log(error);
-          setEmailError("Please enter your EO email or reach your EO Chapter");
-        }
-        if(data){
-          const found = data.find(({EmailID}) => 
-            
-            EmailID === email
-          );
-          console.log(found,'found');
-          if(found){
-            localStorage.setItem('email',email);
-            navigate('/layout');
-          }
-          else
-          {
-          setEmailError("Please enter your EO email or reach your EO Chapter");
-          }
-        }
+    if (error) {
+      console.log(error);
+      setEmailError("Please enter your EO email or reach your EO Chapter");
     }
+    if (data) {
+      const found = data.find(({ emailId }) => emailId === email);
+      console.log(found, "found");
+      if (found) {
+        localStorage.setItem("email", email);
+        navigate("/layout");
+      } else {
+        setEmailError("Please enter your EO email or reach your EO Chapter");
+      }
+    }
+  };
 
   const handleSubmit = (event) => {
     event.preventDefault();
     // console.log(email,'email', checked,'checked');
-    if(email === " " || email === null){
+    if (email === " " || email === null) {
       setEmailError("Please enter email");
       return;
     }
-    if(checked === false){
+    if (checked === false) {
       setError("Please accept disclaimer");
       return;
     }
-    if(email && checked){
+    if (email && checked) {
       fetchData();
     }
   };
@@ -87,13 +84,12 @@ export default function SignIn() {
   const handleEmailChange = (e) => {
     setEmail(e.target.value);
     setEmailError(" ");
-  }
-
+  };
 
   const handleCheckChange = (e) => {
     setChecked(e.target.checked);
     setError(" ");
-  }
+  };
 
   return (
     <ThemeProvider theme={defaultTheme}>
@@ -102,19 +98,24 @@ export default function SignIn() {
         <Box
           sx={{
             marginTop: 8,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-            justifyContent: 'center'
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "center",
           }}
         >
-          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+          <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
             <LockOutlinedIcon />
           </Avatar>
           <Typography component="h1" variant="h5">
             Rie-Kol
           </Typography>
-          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1, width:'22vw'}}>
+          <Box
+            component="form"
+            onSubmit={handleSubmit}
+            noValidate
+            sx={{ mt: 1, width: "22vw" }}
+          >
             <TextField
               margin="normal"
               fullWidth
@@ -124,15 +125,15 @@ export default function SignIn() {
               autoComplete="email"
               autoFocus
               placeholder="Enter EO Global website/app login email (EO Hub)"
-              onChange={(e)=>handleEmailChange(e)}
+              onChange={(e) => handleEmailChange(e)}
             />
-              {emailError && 
-              <span style={{color:'red',fontSize:"12px",margin: "5px"}}>
-                {emailError}</span>
-              }
-              <br/>
-            
-            
+            {emailError && (
+              <span style={{ color: "red", fontSize: "12px", margin: "5px" }}>
+                {emailError}
+              </span>
+            )}
+            <br />
+
             {/* <TextField
               margin="normal"
               required
@@ -145,16 +146,18 @@ export default function SignIn() {
             /> */}
             <FormControlLabel
               control={<Checkbox value="remember" color="primary" />}
-              onChange={(e)=>handleCheckChange(e)}
+              onChange={(e) => handleCheckChange(e)}
               label="Remember me"
             />
-            <br/>
-            {error && <span style={{color:'red',fontSize: "12px"}}>{error}</span>}
+            <br />
+            {error && (
+              <span style={{ color: "red", fontSize: "12px" }}>{error}</span>
+            )}
             <Button
               type="submit"
               fullWidth
               variant="contained"
-              sx={{ mt: 3, mb: 2 , fontWeight: "bold"}}
+              sx={{ mt: 3, mb: 2, fontWeight: "bold" }}
             >
               Proceed
             </Button>
