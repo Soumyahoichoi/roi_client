8f3e77540a3f41ba97b493b13153ee3f1b0a2407
avirupdutta-zeron <avirup@zeron.one>
2023-06-23 13:13:54 +0530

    fix

--- src/Components/paymentSuccess.js
+++ src/Components/paymentSuccess.js
@@ -17,7 +17,9 @@ const PaymentSuccessPage = () => {
   };
 
   useEffect(() => {
-    updateOrderStatus();
+    if (parsed.currency.toLowerCase() === "inr") {
+      updateOrderStatus();
+    }
   }, [JSON.stringify(parsed)]);
 
   return (
