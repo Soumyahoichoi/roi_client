37992bda8435c0fad0a61425d3d141affe8627a1
avirupdutta-zeron <avirup@zeron.one>
2023-06-23 19:38:37 +0530

    fix

--- src/config/supabaseClient.js
+++
@@ -1,7 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-
-const supabaseAnonKey = process.env.REACT_APP_API_KEY;
-const supabaseUrl = process.env.REACT_APP_BASE_URL;
-
-const supabase = createClient(supabaseUrl, supabaseAnonKey);
-export default supabase;
